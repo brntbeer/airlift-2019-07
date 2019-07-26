f6d9a5a0f12947fff9bf82b0f912f3212581f79c
Brent Beer <brent.beer@gmail.com>
2019-07-26 13:17:19 -0700

    Update terraform-launch.sh

---
+++ terraform-launch.sh
@@ -0,0 +1,18 @@
+# Output all commands run and fail if any fail
+set -e -x
+
+cd aks_advnet_rbac
+
+# Init terraform using the defined storage account 
+terraform init \
+  -backend-config="storage_account_name=${TFSTATE_STORAGE}"
+  
+# Generate a terraform plan file
+terraform plan \
+  -input=false \
+  -var="ARM_CLIENT_ID=${ARM_CLIENT_ID}" \
+  -var="ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}" \
+  -out=plan.tfplan
+  
+# Apply the terraform plan
+terraform apply plan.tfplan
